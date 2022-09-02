c9534c83439c946525bca991fc47b90d46191cb5
Richard JEON <ztzy1907@gmail.com>
2022-09-03 00:22:08 +0900

    feat: 필터 설정을 통한 Etag 적용

--- cache/src/main/java/com/example/etag/EtagFilterConfiguration.java
+++ cache/src/main/java/com/example/etag/EtagFilterConfiguration.java
@@ -1,12 +1,23 @@
 package com.example.etag;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class EtagFilterConfiguration {
+public class EtagFilterConfiguration implements WebMvcConfigurer {
 
-//    @Bean
-//    public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
-//        return null;
-//    }
+    @Bean
+    public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
+        FilterRegistrationBean<ShallowEtagHeaderFilter> registrationBean
+                = new FilterRegistrationBean<>();
+
+        registrationBean.setFilter(new ShallowEtagHeaderFilter());
+        registrationBean.addUrlPatterns("/etag", "/resources/*");
+        registrationBean.setOrder(1);
+
+        return registrationBean;
+    }
 }
