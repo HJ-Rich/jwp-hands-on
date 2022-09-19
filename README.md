e4229db69a0613f4cb6c998c49460c8e928bdc64
Richard JEON <ztzy1907@gmail.com>
2022-09-20 05:04:11 +0900

    test: Junit3TestRunner 구현

--- README.md
+++ README.md
@@ -3,8 +3,18 @@
 ## 만들면서 배우는 스프링 실습 코드
 
 ### 학습 순서
+
 - cache
 - thread
 - servlet
 - reflection
 - di
+
+## Reflection
+
+- [x] Junit3TestRunner 클래스의 모든 테스트를 통과시킨다.
+- [ ] Junit4TestRunner 클래스의 모든 테스트를 통과시킨다.
+- [ ] ReflectionTest 클래스의 모든 테스트를 통과시킨다.
+- [ ] ReflectionsTest 클래스의 모든 테스트를 통과시킨다.
+
+<br>
--- reflection/src/test/java/reflection/Junit3TestRunner.java
+++ reflection/src/test/java/reflection/Junit3TestRunner.java
@@ -1,13 +1,38 @@
 package reflection;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
 
     @Test
     void run() throws Exception {
-        Class<Junit3Test> clazz = Junit3Test.class;
-
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        final ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        // given
+        final Class<Junit3Test> clazz = Junit3Test.class;
+        final String expected = String.join(System.lineSeparator(), "Running Test1", "Running Test2", "");
+
+        // when
+        final Junit3Test junit3Test = clazz.getDeclaredConstructor().newInstance();
+        final List<Method> methodsStartsWithTest = Arrays.stream(clazz.getDeclaredMethods())
+                .filter(method -> method.getName().startsWith("test"))
+                .collect(Collectors.toList());
+
+        for (Method method : methodsStartsWithTest) {
+            method.invoke(junit3Test);
+        }
+
+        // then
+        assertThat(out.toString()).isEqualTo(expected);
     }
 }
