266e699117e450058be3493ff63dec1e8a78af64
Richard JEON <ztzy1907@gmail.com>
2022-09-28 16:43:35 +0900

    test: 4단계 구현

--- di/src/test/java/nextstep/study/di/stage4/annotations/ClassPathScanner.java
+++ di/src/test/java/nextstep/study/di/stage4/annotations/ClassPathScanner.java
@@ -1,10 +1,24 @@
 package nextstep.study.di.stage4.annotations;
 
+import java.lang.annotation.Annotation;
+import java.util.HashSet;
 import java.util.Set;
+import org.reflections.Reflections;
 
 public class ClassPathScanner {
 
-    public static Set<Class<?>> getAllClassesInPackage(final String packageName) {
-        return null;
+    public static Set<Class<?>> getAllClassesInPackage(final String rootPackageName,
+                                                       final Class<? extends Annotation>... annotations) {
+        final Set<Class<?>> classes = new HashSet<>();
+        final Reflections reflections = new Reflections(rootPackageName);
+
+        for (Class<? extends Annotation> annotation : annotations) {
+            classes.addAll(reflections.getTypesAnnotatedWith(annotation));
+        }
+
+        return classes;
+    }
+
+    private ClassPathScanner() {
     }
 }
--- di/src/test/java/nextstep/study/di/stage4/annotations/DIContext.java
+++ di/src/test/java/nextstep/study/di/stage4/annotations/DIContext.java
@@ -1,6 +1,12 @@
 package nextstep.study.di.stage4.annotations;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.NoSuchElementException;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
@@ -9,16 +15,57 @@ class DIContext {
 
     private final Set<Object> beans;
 
-    public DIContext(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+    public static DIContext createContextForPackage(final String rootPackageName) {
+        return new DIContext(ClassPathScanner.getAllClassesInPackage(rootPackageName, Service.class, Repository.class));
     }
 
-    public static DIContext createContextForPackage(final String rootPackageName) {
-        return null;
+    private DIContext(final Set<Class<?>> classes) {
+        this.beans = createBeans(classes);
+        this.beans.forEach(this::setFields);
+    }
+
+    private Set<Object> createBeans(final Set<Class<?>> classes) {
+        return classes.stream()
+                .map(this::newInstance)
+                .collect(Collectors.toUnmodifiableSet());
+    }
+
+    private Object newInstance(final Class<?> clazz) {
+        try {
+            final Constructor<?> constructor = clazz.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            return constructor.newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
+                 NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void setFields(final Object bean) {
+        final Class<?> clazz = bean.getClass();
+        final Field[] fields = clazz.getDeclaredFields();
+
+        Arrays.stream(fields)
+                .filter(field -> field.isAnnotationPresent(Inject.class))
+                .collect(Collectors.toList())
+                .forEach(field -> inject(bean, field));
+    }
+
+    private void inject(final Object bean, final Field field) {
+        field.setAccessible(true);
+
+        try {
+            field.set(bean, getBean(field.getType()));
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
-        return null;
+        return (T) beans.stream()
+                .filter(aClass::isInstance)
+                .findAny()
+                .orElseThrow(NoSuchElementException::new);
     }
 }
